c21a339eacd4dee84f783b06dee52908ade32707
ifv2 <ifv2@dmz.org.br>
2024-05-15 18:35:18 -0300

    creating the person class and its attributes

--- AppListaCurso/app/src/main/java/devandroid/flavian/applisatcurso/model/Curso.java
+++
@@ -1,4 +0,0 @@
-package devandroid.flavian.applisatcurso.model;
-
-public class Curso {
-}
--- AppListaCurso/app/src/main/java/devandroid/flavian/applisatcurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/flavian/applisatcurso/view/MainActivity.java
@@ -9,8 +9,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import devandroid.flavian.applisatcurso.R;
+import devandroid.flavian.applisatcurso.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
+
+    Pessoa pessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +25,13 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        pessoa = new Pessoa();
+
+        pessoa.setFirstName("Flavian");
+        pessoa.setSurName("Ventura");
+        pessoa.setDesiredCourse("Android");
+        pessoa.setPhoneNumber("81988434830");
+
     }
 }
